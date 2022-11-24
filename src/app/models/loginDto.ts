e557302070092715fd894c4b0753fcd9a36a6ec0
ANIL KAVUK <36512716+MrKavuk@users.noreply.github.com>
2022-11-25 00:45:21 +0300

    Login Reactive form created

---
+++ src/app/models/loginDto.ts
@@ -0,0 +1,6 @@
+export interface LoginDto {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AppStoreState } from 'src/app/store/app.state';
+import { LoginService } from 'src/app/services/login.service';
+import { Store } from '@ngrx/store';
+import { ToastrMessageService } from 'src/app/services/toastr-message.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  loginForm!: FormGroup;
+  constructor(
+    private loginService: LoginService,
+    private store: Store<AppStoreState>,
+    private toastr: ToastrMessageService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    this.loginForm();
+    this.loginDtoForm();
   }
-  loginForm() {}
+  loginDtoForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
 }
